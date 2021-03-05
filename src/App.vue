1a04cd7f0d465d77e6497cd5ad989dcb093cb5c3
Thomas Gline <glinethomas20@gmail.com>
2021-03-05 10:01:47 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <headerList v-bind:listElement="listElement"> </headerList>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1></h1>
+          <h2></h2>
+          <form class="form-group">
+            <label for="list"></label>
+            <input
+              type="text"
+              class="form-control"
+              id="list"
+              placeholder="écrire ici.."
+              v-model="element"
+            />
+            <button class="btn" v-on:click.prevent="doThat">add</button>
+            <div>
+              <ul>
+                <li v-for="(element, index) in listElement" :key="index">
+                  {{ element }}
+                  <button class="btn" v-on:click.prevent="deleteThat(index)">
+                    x
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderList from "./components/HeaderList.vue";
+
+export default {
+  data() {
+    return {
+      listElement: [],
+      element: "",
+    };
+  },
+  methods: {
+    doThat: function () {
+      this.listElement.push(this.element);
+      console.log(this.listElement);
+    },
+    deleteThat: function (index) {
+      this.listElement.splice(index, 1);
+    },
+  },
+  name: "App",
+  components: {
+    headerList: HeaderList,
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: #9cdcc4;
+}
+button {
+  background-color: #41b783 !important;
+}
+</style>
---
+++ src/components/HeaderList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <header>Workshop : "To do List with Vue.js</header>
+    <div class="imageHeader"><img src=".././assets/logo_vue.png" /></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["listElement"],
+  name: "HeaderList",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  background-color: #34495e;
+  width: 100%;
+  min-height: 5rem;
+  text-align: center;
+  line-height: 5rem;
+  font-size: 1.5rem;
+  color: white;
+}
+img {
+  position: absolute;
+  top: 0px;
+}
+</style>
